46f3b4c0db650a45ad9642f82c3f19a2764fd901
mileyanda <yandaqa@gmail.com>
2017-01-24 15:25:20 +0700

    Add project files.

---
+++ Tugas1/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+namespace Tugas1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
